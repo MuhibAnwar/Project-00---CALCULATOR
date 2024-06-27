a23da098d38c7ecd184561da8d0495f6c6950829
Muhib Anwar <160424220+MuhibAnwar@users.noreply.github.com>
2024-06-28 00:27:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your name : ", type: "number", name: "first" },
+    { message: "Enter your name : ", type: "number", name: "second" },
+    {
+        message: "Select the operator you want to perform : ",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    }
+]);
+if (answer.operator == "addition") {
+    console.log(answer.first + answer.second);
+}
+else if (answer.operator == "subtraction") {
+    console.log(answer.first - answer.second);
+}
+else if (answer.operator == "multiplication") {
+    console.log(answer.first * answer.second);
+}
+else if (answer.operator == "division") {
+    console.log(answer.first / answer.second);
+}
+else {
+    console.log("invalid operator");
+}
